925014f7f025b1181a7cd207d419a1a1a840cbde
DanaUni <63239336+DanaUni@users.noreply.github.com>
2022-01-24 10:57:24 +0100

    Add files via upload

--- dataframe_to_conll.py
+++ dataframe_to_conll.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 infile_path = "..\data\SEM-2012-SharedTask-CD-SCO-training-simple.v2_SHORT-out.txt"
 outfile_path = "..\data\SEM-2012-SharedTask-CD-SCO-training-simple.v2_SHORT-out-CONLL.conll"
-dataframe = pd.read_csv(infile_path)
+dataframe = pd.read_csv(infile_path, encoding="utf-8")
 
 def convert_dataframe_to_conll(dataframe, outfile_path, delimiter= '\t'):
     """
@@ -12,16 +12,15 @@ def convert_dataframe_to_conll(dataframe, outfile_path, delimiter= '\t'):
     :param outfile_path: path of the output file
     :param delimiter: delimiter to separate the columns of the dataframe in the conll format
     """
-
     with open(outfile_path, 'w') as outputfile:
-        for i in range(len(dataframe) - 1):
+        for i in range(len(dataframe)):
             row = dataframe.iloc[i]
             row = [str(j) for j in row]
             row = delimiter.join(row)
 
-            if dataframe.loc[i + 1, "token_nr"] == 0:
-                outputfile.write(row + "\n")
+            if (dataframe.loc[i, "token_nr"] == 0) and (i != 0):
                 outputfile.write("\n")
+                outputfile.write(row + "\n")
             else:
                 outputfile.write(row + "\n")
 
